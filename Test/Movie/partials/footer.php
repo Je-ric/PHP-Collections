240b148bba5650c82bad7e7bb6f6388eb69d65ed
Je-ric <delacruz.jeric.j@gmail.com>
2025-09-07 22:26:44 +0800

    remove uneeded

--- Test/Movie/partials/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer footer-center p-4 bg-primary-bg text-text-muted border-t border-border-color">
-    <div>
-        <p>© <?= date('Y') ?> Movie Recommendation System • Created by JMR</p>
-    </div>
-</footer>
